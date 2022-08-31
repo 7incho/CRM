b699d50737e60658b3431ef29ab4a3aa29a76a9e
Nacho386 <nachosilva388@gmail.com>
2022-08-31 05:39:35 -0300

    Avances del login, ya estan las funciones básicas

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {ContextProvider} from './contexts/ContextProvider';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain=process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId=process.env.REACT_APP_AUTH0_CLIENT_ID;
+
+console.log(domain, clientId);
 
 ReactDOM.render(
-    <ContextProvider>
-    <App />
+    <ContextProvider >
+        <Auth0Provider 
+        domain={domain} 
+        clientId={clientId} 
+        redirectUri={window.location.origin} 
+        >
+            <App />
+        </Auth0Provider>
+    
     </ContextProvider>, document.getElementById('root')
     );
